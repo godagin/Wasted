99f3367785e6a63e52b5d65a47e4ad0c9b1c6658
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-09-15 20:47:56 +0300

    create class food

---
+++ Wasted/Food.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Wasted
+{
+    class Food
+    {
+        private string foodName;
+        private string foodDescription;
+        private double fullPrice;
+        private int amount;
+
+
+        public Food()
+        {
+            FoodName = this.foodName;
+            FoodDescription = this.foodDescription;
+            FullPrice = this.fullPrice;
+            Amount = this.amount;
+        }
+
+
+        public string FoodName { get; set; }
+        public string FoodDescription { get; set; }
+        public double FullPrice { get; set; }
+        public int Amount { get; set; }
+
+
+       public double CalcPrice(int discountPercentage)
+        {
+            double price;
+            price = fullPrice * (1 - discountPercentage / 100);
+            return price;
+        }
+
+        public string SetExpDate (int days)
+        {
+            return DateTime.Now.AddDays(days).ToString("dd.MM.yy");
+        }
+
+        //pasidaryt su kiekiu, kad nepaimtu daugiau nei yra
+        // viena su int, kita su double
+        public int TakeFood (int take)
+        {
+            
+            return amount - take;
+        }
+
+        
+    }
+}
