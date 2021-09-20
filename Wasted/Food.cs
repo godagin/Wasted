5b2a40724ca6f241520b23d29ef18ca21a077539
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-09-20 20:16:13 +0300

    fix constructor

--- Wasted/Food.cs
+++ Wasted/Food.cs
@@ -4,31 +4,26 @@ namespace Wasted
 {
     class Food
     {
-        private string foodName;
-        private string foodDescription;
-        private double fullPrice;
-        private int amount;
-
-
-        public Food()
+       
+        public Food(string foodName, string foodDescription, double fullPrice, double amount)
         {
-            FoodName = this.foodName;
-            FoodDescription = this.foodDescription;
-            FullPrice = this.fullPrice;
-            Amount = this.amount;
+            this.FoodName = foodName;
+            this.FoodDescription = foodDescription;
+            this.FullPrice = fullPrice;
+            this.Amount = amount;
         }
 
 
         public string FoodName { get; set; }
         public string FoodDescription { get; set; }
         public double FullPrice { get; set; }
-        public int Amount { get; set; }
+        public double Amount { get; set; }
 
 
        public double CalcPrice(int discountPercentage)
         {
             double price;
-            price = fullPrice * (1 - discountPercentage / 100);
+            price = FullPrice * (1 - discountPercentage / 100);
             return price;
         }
 
@@ -37,15 +32,11 @@ namespace Wasted
             return DateTime.Now.AddDays(days).ToString("dd.MM.yy");
         }
 
-        //if amount is an integer
-        public int TakeFood (int take)
-        {
-            return amount - take;
-        }
+        
         //if amount is kg, g
         public double TakeFood(double take)
         {
-           return amount - take;
+           return Amount - take;
         }
 
     }
