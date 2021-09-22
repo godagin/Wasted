ffaef7a0b7e62a4c357f4cd09495a27647103e60
godagin <goda.gintalaite@gmail.com>
2021-09-22 18:13:13 +0300

    upload new elements of Form1

--- Wasted/Form1.cs
+++ Wasted/Form1.cs
@@ -17,5 +17,20 @@ namespace Wasted
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        private void offer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
