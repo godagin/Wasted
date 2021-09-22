4cff754695b2610da5c1ee2eb3121d1d6ffacf19
kamile171 <76160527+kamile171@users.noreply.github.com>
2021-09-22 21:50:03 +0300

    add sort classes

---
+++ Wasted/sort.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace Wasted
+{
+     class AlphabeticSortAToZ : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            return string.Compare(x.FoodName, y.FoodName);
+        }
+    }
+    class AlphabeticSortZToA : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            return string.Compare(x.FoodName, y.FoodName) * (-1);
+        }
+    }
+    class AmountSortHighToLow : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            if (x.Amount == y.Amount)
+                return string.Compare(x.FoodName, y.FoodName);
+            else if (x.Amount > y.Amount)
+                return 1;
+            else if (x.Amount < y.Amount)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    class AmountSortLowToHigh : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            if (x.Amount == y.Amount)
+                return string.Compare(x.FoodName, y.FoodName);
+            else if (x.Amount < y.Amount)
+                return 1;
+            else if (x.Amount > y.Amount)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
