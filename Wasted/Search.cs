2c0eae5ad5467a34e5eeb45dc917857dc09b966e
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-09-23 08:17:18 +0300

    move methods to search class

---
+++ Wasted/Search.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Wasted
+{
+    class Search
+    {
+        public bool MatchesName(string input, Food food)
+        {
+            bool result = Regex.IsMatch(food.FoodName, @"(^|;|,)" + Regex.Escape(input) + @"($|;|,)");
+
+            return result;
+        }
+
+        public bool MatchesInDescription(string input, Food food)
+        {
+            bool result = Regex.IsMatch(food.FoodDescription, @"(^|;|,)" + Regex.Escape(input) + @"($|;|,)");
+
+            return result;
+        }
+    }
+}
