4697495302324d984ce9be2b617b276376c5dd00
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-09-28 12:58:30 +0300

    fix DB primary key

---
+++ Wasted/Migrations/202109271516512_ChangeButton1_Click.cs
@@ -0,0 +1,24 @@
+﻿namespace Wasted.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeButton1_Click : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.Foods");
+            AlterColumn("dbo.Foods", "FoodName", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.Foods", "FoodName");
+            
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Foods", "ID", c => c.Int(nullable: false, identity: true));
+            DropPrimaryKey("dbo.Foods");
+            AlterColumn("dbo.Foods", "FoodName", c => c.String());
+            
+        }
+    }
+}
