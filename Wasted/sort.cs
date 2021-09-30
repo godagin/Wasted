5979e799011aae8024e707f6c97c2e7a546137ed
kamile171 <76160527+kamile171@users.noreply.github.com>
2021-09-30 20:25:42 +0300

    add price and exp date sort classes

--- Wasted/sort.cs
+++ Wasted/sort.cs
@@ -3,27 +3,45 @@ using System.Collections.Generic;
 
 namespace Wasted
 {
-     class AlphabeticSortAToZ : IComparer<Food>
+    class AlphabeticSortAToZ : IComparer<Food>
     {
         public int Compare(Food x, Food y)
         {
-            return string.Compare(x.FoodName, y.FoodName);
+            if (x.FoodName == y.FoodName) // Same name -> sort price low to high
+            {
+                if (x.FullPrice > y.FullPrice)
+                    return 1;
+                else if (x.FullPrice < y.FullPrice)
+                    return -1;
+                else
+                    return 0;
+            }
+            else
+                return string.Compare(x.FoodName, y.FoodName);
         }
     }
     class AlphabeticSortZToA : IComparer<Food>
     {
-        public int Compare(Food x, Food y)
+        public int Compare(Food x, Food y) // Same name -> sort price low to high
         {
-            return string.Compare(x.FoodName, y.FoodName) * (-1);
+            if (x.FoodName == y.FoodName)
+            {
+                if (x.FullPrice > y.FullPrice)
+                    return 1;
+                else if (x.FullPrice < y.FullPrice)
+                    return -1;
+                else
+                    return 0;
+            }
+            else
+                return string.Compare(x.FoodName, y.FoodName) * (-1);
         }
     }
     class AmountSortLowToHigh : IComparer<Food>
     {
         public int Compare(Food x, Food y)
         {
-            if (x.Amount == y.Amount)
-                return string.Compare(x.FoodName, y.FoodName);
-            else if (x.Amount > y.Amount)
+            if (x.Amount > y.Amount)
                 return 1;
             else if (x.Amount < y.Amount)
                 return -1;
@@ -35,9 +53,7 @@ namespace Wasted
     {
         public int Compare(Food x, Food y)
         {
-            if (x.Amount == y.Amount)
-                return string.Compare(x.FoodName, y.FoodName);
-            else if (x.Amount < y.Amount)
+            if (x.Amount < y.Amount)
                 return 1;
             else if (x.Amount > y.Amount)
                 return -1;
@@ -45,4 +61,59 @@ namespace Wasted
                 return 0;
         }
     }
+
+    class PriceSortLowToHigh : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            if (x.FullPrice == y.FullPrice) // Same price -> sort name A to Z
+                return string.Compare(x.FoodName, y.FoodName);
+            else if (x.FullPrice > y.FullPrice)
+                return 1;
+            else if (x.FullPrice < y.FullPrice)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    class PriceSortHighToLow : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            if (x.FullPrice == y.FullPrice) // Same price -> sort name A to Z 
+                return string.Compare(x.FoodName, y.FoodName);
+            else if (x.FullPrice < y.FullPrice)
+                return 1;
+            else if (x.FullPrice > y.FullPrice)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    class ExpSortLowToHigh : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            if (x.OfferExpDate > y.OfferExpDate)
+                return 1;
+            else if (x.OfferExpDate < y.OfferExpDate)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    class ExpSortHighToLow : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            if (x.OfferExpDate < y.OfferExpDate)
+                return 1;
+            else if (x.OfferExpDate > y.OfferExpDate)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
 }
