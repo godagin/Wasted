5761463230c73781f65c970384e99852c021697e
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-10-05 11:52:42 +0300

    add exceptions in class ReadingFile

--- Wasted/ReadingFile.cs
+++ Wasted/ReadingFile.cs
@@ -7,35 +7,53 @@ namespace Wasted
 {
     class ReadingFile
     {
-        public void ReadFileTxt(string fileName, List<Food> foodList)
-        {
-            string line;
-            StreamReader file = new StreamReader(fileName);
-            while ((line = file.ReadLine()) != null)
-            {
-                string[] data = line.Split(';');
-                double price = Convert.ToDouble(data[2]);
-                double amount = Convert.ToDouble(data[3]);
-                Food food = new Food(data[0], data[1], price, amount);
-                foodList.Add(food);
-            }
-
-            file.Close();
-        }
-
         public void ReadFileCsv(string fileName, List<Food> foodList)
         {
             string line;
-            StreamReader file = new StreamReader(fileName);
-            while ((line = file.ReadLine()) != null)
+            try
             {
-                string[] data = line.Split(',');
-                double price = Convert.ToDouble(data[2]);
-                double amount = Convert.ToDouble(data[3]);
-                Food food = new Food(data[0], data[1], price, amount);
-                foodList.Add(food);
+                StreamReader file = new StreamReader(fileName);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] data = line.Split(',');
+                    double price = Convert.ToDouble(data[2]);
+                    double amount = Convert.ToDouble(data[3]);
+                    Food food = new Food(data[0], data[1], price, amount);
+                    foodList.Add(food);
+                }
+                file.Close();
             }
-            file.Close();
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Not valid arguments.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File with this name was not found.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong with file reading.");
+            }
+
+
+            /*public void ReadFileTxt(string fileName, List<Food> foodList)
+            {
+                string line;
+                StreamReader file = new StreamReader(fileName);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] data = line.Split(';');
+                    double price = Convert.ToDouble(data[2]);
+                    double amount = Convert.ToDouble(data[3]);
+                    Food food = new Food(data[0], data[1], price, amount);
+                    foodList.Add(food);
+                }
+
+                file.Close();
+            }
+            */
+
         }
     }
 
