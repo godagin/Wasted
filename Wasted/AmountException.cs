bc48abb379f51804696cf7e0161bba1aebf17534
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-10-05 18:53:50 +0300

    extend FoodException class with AmountException

--- Wasted/AmountException.cs
+++ Wasted/AmountException.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Wasted
 {
-    class NotSuitableAmountException : Exception
+    class NotSuitableAmountException : FoodException
     {
         public NotSuitableAmountException() 
         { 
