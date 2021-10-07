75d93821e1870eaeaae93a6aa5cc2363a20eca01
godagin <goda.gintalaite@gmail.com>
2021-10-07 18:03:59 +0300

    add DiscreteFood and WeightedFood classes that extend class Food

---
+++ Wasted/DiscreteFood.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wasted
+{
+    class DiscreteFood : Food
+    {
+        public int Quantity { get; set; }
+       
+        public int TakeFood(int take)
+        {
+            if (Quantity <= take)
+            {
+                //what if Quantity == take, Food Offer must be removed from list
+                Quantity -= take;
+                return take; /// maybe return a new object with new quantity?
+            }
+            else
+            {
+                //same poroblem with Quantity = 0, offer must be removed
+                int quantityTemp = Quantity;
+                Quantity = 0;
+                return quantityTemp;
+                ///throw new NotSuitableAmountException(); // fix after merge
+            }
+            
+        }
+    }
+}
---
+++ Wasted/WeighedFood.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wasted
+{
+    class WeighedFood
+    {
+        public double Weight { get; set; }
+
+        public double TakeFood(double take)
+        {
+            if (Weight <= take)
+            {
+                //what if Quantity == take, Food Offer must be removed from list
+                Weight -= take;
+                return take; /// maybe return a new object with new quantity?
+            }
+            else
+            {
+                //same poroblem with Quantity = 0, offer must be removed
+                double quantityTemp = Weight;
+                Weight = 0;
+                return quantityTemp;
+                ///throw new NotSuitableAmountException(); // fix after merge
+            }
+
+        }
+    }
+}
