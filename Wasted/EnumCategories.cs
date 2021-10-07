132b0b7afc4388cbc9308f8bded0e4bae92958ff
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-10-07 15:44:40 +0300

    add bakery and confectionery to enum

--- Wasted/EnumCategories.cs
+++ Wasted/EnumCategories.cs
@@ -8,7 +8,7 @@ namespace Wasted
     {
         [Flags]
         enum FoodType { Vegetables = 1, Fruits = 2, Fish_and_Seafood = 3, Meat_and_Poultry = 4, Dairy = 5, Grains_Beans_and_Nuts = 6,
-            Sweets = 7, Soups = 8, Meals = 9, Other = 10 };
+            Sweets = 7, Soups = 8, Meals = 9, Bakery = 10, Confectionery = 11, Other = 12 };
 
     }
 }
