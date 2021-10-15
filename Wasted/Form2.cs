4801f97e87a94727ef24208c7c38a23f0ae8fa6c
kamile171 <76160527+kamile171@users.noreply.github.com>
2021-10-15 11:54:09 +0300

    fix expDays parse

--- Wasted/Form2.cs
+++ Wasted/Form2.cs
@@ -32,11 +32,23 @@ namespace Wasted
             {
                 if (RB_type_discrete.Checked && !RB_type_weighted.Checked && isAmountValid)
                 {
+                    if (textBoxExpiration.Text == "")
+                    {
                         food = new DiscreteFood(textBoxName.Text,
                         textBoxDescription.Text,
                         Double.Parse(textBoxPrice.Text),
-                        int.Parse(textBoxQuantity.Text)
+                        int.Parse(textBoxQuantity.Text));
+                    }
+                    else
+                    {
+                        food = new DiscreteFood(textBoxName.Text,
+                        textBoxDescription.Text,
+                        Double.Parse(textBoxPrice.Text),
+                        int.Parse(textBoxQuantity.Text),
                         int.Parse(textBoxExpiration.Text));
+
+                    }
+                        
 
                     DatabaseHandler.GetHandler().AddItemToFoodTable(food); //add entity to the add method
 
@@ -44,12 +56,22 @@ namespace Wasted
                 }
                 else if (!RB_type_discrete.Checked && RB_type_weighted.Checked && isWeightValid)
                 {
-                    food = new WeighedFood(textBoxName.Text,
+                    if (textBoxExpiration.Text == "")
+                    {
+                        food = new DiscreteFood(textBoxName.Text,
                         textBoxDescription.Text,
                         Double.Parse(textBoxPrice.Text),
-                        Double.Parse(textBoxWeight.Text),
+                        int.Parse(textBoxQuantity.Text));
+                    }
+                    else
+                    {
+                        food = new DiscreteFood(textBoxName.Text,
+                        textBoxDescription.Text,
+                        Double.Parse(textBoxPrice.Text),
+                        int.Parse(textBoxQuantity.Text),
                         int.Parse(textBoxExpiration.Text));
 
+                    }
                     DatabaseHandler.GetHandler().AddItemToFoodTable(food); //add entity to the add method
 
                     FoodList.GetObject().AddCreatedFood(food);
