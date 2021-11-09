22ac4680fc022a8960344ddb11f934e370d6a639
godagin <goda.gintalaite@gmail.com>
2021-11-09 22:50:55 +0200

    fix error

--- WebWasted/Controllers/CartController.cs
+++ WebWasted/Controllers/CartController.cs
@@ -18,35 +18,44 @@ namespace WebWasted.Controllers
     [Route("api/cart")]
     public class CartController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<Food> Get()
+
+        private readonly IConfiguration _configuration;
+
+        public CartController(IConfiguration configuration)
         {
-            using (DataContext context = new DataContext())
-            {
-                var userCart = from food in context.Foods 
-                               join user in context.Users on food.BuyerID equals user.ID
-                               where food.BuyerID == user.ID 
-                               select food;
-                return userCart.ToList();
-
-            }
+            _configuration = configuration;
         }
+        /*
+                [HttpGet]
+                public IEnumerable<Food> Get()
+                {
+                    using (DataContext context = new DataContext())
+                    {
+                        var userCart = from food in context.Foods 
+                                       join user in context.Users on food.BuyerID equals user.ID
+                                       where food.BuyerID == user.ID 
+                                       select food;
+                        return userCart.ToList();
 
+                    }
+                }
+        */
         [HttpPost]
         public int Post([FromBody] CartArguments IDS)
         {
             Console.WriteLine("veikia????");
             using (DataContext context = new DataContext())
             {
+                Console.WriteLine("veikia????");
                 var findFood = (from food in context.Foods where food.ID == IDS.foodID select food).First();
-
+                Console.WriteLine("veikia????");
                 if ((findFood == null) || (IDS.userID == findFood.OwnerID))
                 {
                     return -1;
                 }
-
+                Console.WriteLine("veikia????");
                 findFood.BuyerID = IDS.userID;
-                
+                Console.WriteLine("veikia????");
                 return findFood.BuyerID;
             }
         }
