b542110a6f3c2f9e92fd04ec4f02295b1e1d27ca
kamile171 <76160527+kamile171@users.noreply.github.com>
2021-11-11 23:51:46 +0200

    remove unnecessary comments

--- WebWasted/Controllers/ContactsController.cs
+++ WebWasted/Controllers/ContactsController.cs
@@ -22,32 +22,22 @@ namespace WebWasted.Controllers
         [HttpGet("{id}")]
         public String Get(int id)
         {
-            String email = "";
-            Console.WriteLine("test1");
 
             using (DataContext context = new DataContext())
             {
-                Console.WriteLine("test2");
-                var owner = (from user in context.Users where user.ID == id select user).FirstOrDefault();
+                var owner = (from user in context.Users where user.ID == id select user).First();
 
                 if (owner == null)
                 {
-                    Console.WriteLine("test3");
-
-                    email = "no email";
+                    return "";
                 }
-                else
-                {
-                    Console.WriteLine("test4");
-                    Console.WriteLine(owner.ContactEmail);
-                    email = owner.ContactEmail;
-                }
-
-                return email;
+ 
+                Console.WriteLine(owner.ContactEmail);
+ 
+                return owner.ContactEmail;
 
             }
         }
-
 
     }
 }
