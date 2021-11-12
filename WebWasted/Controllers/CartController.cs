10982611573653d96fdc926c9bedc9b8e89f7fe5
godagin <goda.gintalaite@gmail.com>
2021-11-12 02:31:55 +0200

    add bracket

--- WebWasted/Controllers/CartController.cs
+++ WebWasted/Controllers/CartController.cs
@@ -25,30 +25,28 @@ namespace WebWasted.Controllers
         {
             _configuration = configuration;
         }
-        
+
         [HttpGet("{id}")]
         public IEnumerable<Food> Get(int id)
         {
-               
-                var userCart = from food in DatabaseHandler.Instance.dc.Foods
-                               where food.BuyerID == id
-                               select food;
 
-                return userCart.ToList();
-            
+            var userCart = from food in DatabaseHandler.Instance.dc.Foods
+                           where food.BuyerID == id
+                           select food;
+
+            return userCart.ToList();
+
         }
-        
+
         [HttpPost]
         public int Post([FromBody] CartArguments IDS)
         {
-          
-
             Food findItem = DatabaseHandler.Instance.dc.Foods.ToList().Find(
                 delegate (Food item)
                 {
                     return item.ID == IDS.foodID;
                 }
-             );
+            );
 
             if (findItem != null)
             {
@@ -56,13 +54,13 @@ namespace WebWasted.Controllers
                 DatabaseHandler.Instance.dc.SaveChanges();
             }
             else
-            {  
+            {
                 Console.WriteLine("not found");
                 return -1;
             }
 
             return findItem.BuyerID;
-          }
+        }
 
-   
+    }
 }
