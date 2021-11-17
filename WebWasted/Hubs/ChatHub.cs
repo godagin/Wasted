9d9f0f320cbe5157d3969f1163c10a2b535f70a2
kamile171 <76160527+kamile171@users.noreply.github.com>
2021-11-17 19:22:59 +0200

    add missing file

---
+++ WebWasted/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebWasted.Models;
+
+namespace WebWasted.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string user, string message)
+        {
+            await Clients.All.SendAsync("Send", user, message);
+        }
+
+    }
+}
