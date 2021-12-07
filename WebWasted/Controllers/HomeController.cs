7346ecbbd34af266ca9db38f377e50f3fd955cef
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-12-07 16:51:30 +0200

    changesForQuality

---
+++ WebWasted/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebWasted.Services;
+
+namespace WebWasted
+{
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IDataContext _dataContext;
+        public HomeController(IConfiguration configuration, IDataContext dataContext)
+        {
+            _configuration = configuration;
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Food> GetFirst()
+        {
+            ItemService itemService = new ItemService(_dataContext);
+            return itemService.GetFirstOffers().ToList();
+        }
+    }
+}
