950da91ea380fb2e5b94d8c561400bfa0250bbe6
Paulina <78165114+paulinaa002@users.noreply.github.com>
2021-12-09 09:07:12 +0200

    delete action

--- react-wasted/src/CreateOffer.js
+++ react-wasted/src/CreateOffer.js
@@ -1,13 +1,13 @@
-import React,{Component} from 'react';
-import {Form, Button, Row, Col} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
-export class CreateOffer extends Component{
-    constructor(props){
+export class CreateOffer extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             PhotoFileName: "anonymous.jpg",
             PhotoPath: process.env.REACT_APP_API + '/Photos/',
-            input:{
+            input: {
                 type: 1,
                 name: "",
                 description: "",
@@ -20,121 +20,120 @@ export class CreateOffer extends Component{
     }
 
     inputHandler = (event) => {
-        switch(event.target.id){
-            case "formGridName" :
+        switch (event.target.id) {
+            case "formGridName":
                 this.state.input.name = event.target.value;
                 break;
-            case "formGridDescription" :
+            case "formGridDescription":
                 this.state.input.description = event.target.value;
                 break;
-            case "formGridCategory" :
+            case "formGridCategory":
                 this.state.input.foodType = event.target.value;
                 break;
-            case "formGridPrice" :
+            case "formGridPrice":
                 this.state.input.price = event.target.value;
                 break;
-            case "formGridAmount" :
+            case "formGridAmount":
                 this.state.input.amount = event.target.value;
                 break;
-            case "formGridExpiration" :
+            case "formGridExpiration":
                 this.state.input.expiration = event.target.value;
                 break;
-            case "formGridType" :
+            case "formGridType":
                 this.state.input.type = event.target.value;
                 break;
         }
-        this.setState((state) => {return state;});
+        this.setState((state) => { return state; });
     }
 
-    imageUpload=(e)=>{
+    imageUpload = (e) => {
         e.preventDefault();
 
-        const formData=new FormData();
+        const formData = new FormData();
         formData.append("file", e.target.files[0], e.target.files[0].name);
 
         fetch(process.env.REACT_APP_API + '/api/foods/SaveFile', {
             method: 'POST',
             body: formData
         })
-        .then(res=>res.json())
-        .then(data=>{
-            this.setState({PhotoFileName:data});
-        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ PhotoFileName: data });
+            })
     }
 
     radioButtonHandler = (e) => {
-        if(e.target.id == "weighedRadio" && e.target.checked){
+        if (e.target.id == "weighedRadio" && e.target.checked) {
             this.state.input.type = 1;
-        } else if(e.target.id == "discreteRadio" && e.target.checked){
+        } else if (e.target.id == "discreteRadio" && e.target.checked) {
             this.state.input.type = 2;
         }
-        this.setState((state) => {return state});
+        this.setState((state) => { return state });
     }
 
     submitHandler = (event) => {
         const requestOptions = {
-            method: 'POST', 
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ 
-                type: this.state.input.type,
-                owner: localStorage.getItem('userID'),
-                name: this.state.input.name,
-                fullPrice: this.state.input.price, 
-                description: this.state.input.description,
-                amount: this.state.input.amount,
-                foodType: this.state.input.foodType,
-                expTime: this.state.input.expiration,
-                fileName: this.state.PhotoFileName
-             })
-        };
-        fetch(process.env.REACT_APP_API + '/api/foods', requestOptions) 
-            .then(response => {
-                if(response.status == 200){
-                    window.location.reload(false);
-                } else{
-                    console.log("ERRROR!!!!!!!!!!");
-                }
-            });
-    }
-
-    /* updateClick(){
-        fetch(process.env.REACT_APP_API + '/api/foods',{
             method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 type: this.state.input.type,
                 owner: localStorage.getItem('userID'),
                 name: this.state.input.name,
-                fullPrice: this.state.input.price, 
+                fullPrice: this.state.input.price,
                 description: this.state.input.description,
                 amount: this.state.input.amount,
                 foodType: this.state.input.foodType,
                 expTime: this.state.input.expiration,
                 fileName: this.state.PhotoFileName
             })
-        })
-        .then(response => {
-            if(response.status == 200){
-                window.location.reload(false);
-            } else{
-                console.log("ERRROR!!!!!!!!!!");
-            }
-            
-        })
-        
+        };
+        fetch(process.env.REACT_APP_API + '/api/foods', requestOptions)
+            .then(response => {
+                if (response.status == 200) {
+                    window.location.reload(false);
+                } else {
+                    console.log("ERRROR!!!!!!!!!!");
+                }
+            });
     }
-*/
+
+    updateClick(id) {
+        fetch(process.env.REACT_APP_API + '/api/foods/' + id, {
+            method: 'PUT',
+            body: JSON.stringify({
+                type: this.state.input.type,
+                name: this.state.input.name,
+                fullPrice: this.state.input.price,
+                description: this.state.input.description,
+                amount: this.state.input.amount,
+                foodType: this.state.input.foodType,
+                expTime: this.state.input.expiration,
+                fileName: this.state.PhotoFileName
+            }).then((result) => {
+                    result.json().then((resp) => {
+                        console.warn(resp)
+
+                    })
+                })
+            // .then(response => {
+            //     if(response.status == 200){
+            //         window.location.reload(false);
+            //     } else{
+            //         console.log("ERRROR!!!!!!!!!!");
+            //     }})
+        })
 
 
-    render(){
-        const{
+    }
+
+
+
+    render() {
+        const {
             PhotoFileName,
             PhotoPath
-        }=this.state;
-        return(
+        } = this.state;
+        return (
             <Form >
                 <Row className="mb-3">
                     <Form.Group as={Col} defaultValue={this.state.input.name} controlId="formGridName" onChange={this.inputHandler}>
@@ -146,12 +145,12 @@ export class CreateOffer extends Component{
                         <Form.Label>Description</Form.Label>
                         <Form.Control placeholder="Enter food description" />
                     </Form.Group>
-                    
+
                     <Form.Group as={Col} defaultValue={this.state.input.foodType}>
                         <Form.Label as="legend" column sm={2}>
                             Food category
                         </Form.Label>
-                        <Form.Select  id="formGridCategory" onChange={this.inputHandler}>
+                        <Form.Select id="formGridCategory" onChange={this.inputHandler}>
                             <option value="0">Default</option>
                             <option value="1">Vegetables</option>
                             <option value="2">Fruits</option>
@@ -165,8 +164,8 @@ export class CreateOffer extends Component{
                         </Form.Select>
                     </Form.Group>
                 </Row>
-                
-                
+
+
                 <Row className="mb-3">
                     <Form.Group as={Col} defaultValue={this.state.input.price} controlId="formGridPrice" onChange={this.inputHandler}>
                         <Form.Label>Price</Form.Label>
@@ -175,7 +174,7 @@ export class CreateOffer extends Component{
 
                     <Form.Group as={Col} defaultValue={this.state.input.amount} controlId="formGridAmount" onChange={this.inputHandler}>
                         <Form.Label>Amount</Form.Label>
-                        <Form.Control type = "number" min="0.0" precision={2} placeholder="Enter amount" />
+                        <Form.Control type="number" min="0.0" precision={2} placeholder="Enter amount" />
                     </Form.Group>
 
                     <Form.Group as={Col} defaultValue={this.state.input.expiration} controlId="formGridExpiration" onChange={this.inputHandler}>
@@ -183,44 +182,48 @@ export class CreateOffer extends Component{
                         <Form.Control type="number" placeholder="Enter amount of days" />
                     </Form.Group>
                 </Row>
-                
+
                 <Form.Group as={Row} className="mb-3" controlId="formGridType">
                     <Form.Label as="legend" column sm={6}>
                         Food type by amount
                     </Form.Label>
                     <Col sm={10}>
                         <Form.Check
-                        checked={this.state.input.type == 1 ? true : false}
-                        type="radio"
-                        label="Weighed"
-                        name="typeRadios"
-                        id="weighedRadio"
-                        onChange={this.radioButtonHandler}
+                            checked={this.state.input.type == 1 ? true : false}
+                            type="radio"
+                            label="Weighed"
+                            name="typeRadios"
+                            id="weighedRadio"
+                            onChange={this.radioButtonHandler}
                         />
                         <Form.Check
-                        checked={this.state.input.type == 2 ? true : false}
-                        type="radio"
-                        label="Discrete"
-                        name="typeRadios"
-                        id="discreteRadio"
-                        onChange={this.radioButtonHandler}
+                            checked={this.state.input.type == 2 ? true : false}
+                            type="radio"
+                            label="Discrete"
+                            name="typeRadios"
+                            id="discreteRadio"
+                            onChange={this.radioButtonHandler}
                         />
                     </Col>
                 </Form.Group>
 
                 <div className="p-2 w-50 bd-highlight">
                     <img alt="" width="200px" height="200px"
-                    src={PhotoPath+PhotoFileName}/>
-                    <input className="m-2" type="file" onChange={this.imageUpload}/>
+                        src={PhotoPath + PhotoFileName} />
+                    <input className="m-2" type="file" onChange={this.imageUpload} />
                 </div>
-    
-                <Button onClick={()=>{
+
+                <Button onClick={() => {
                     //patikrinti ar dienos nera skaicius su kableliu
-                    this.setState(() => {return{ createOffer: false,
-                    fileName:PhotoFileName}});
+                    this.setState(() => {
+                        return {
+                            createOffer: false,
+                            fileName: PhotoFileName
+                        }
+                    });
                     this.submitHandler();
                     //this.updateClick();
-                    }}>
+                }}>
                     Submit
                 </Button>
             </Form>
--- react-wasted/src/Foods.js
+++ react-wasted/src/Foods.js
@@ -1,122 +1,115 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { Table } from 'react-bootstrap';
 import { CreateOffer } from './CreateOffer';
 
-export class Foods extends Component{
+export class Foods extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             PhotoPath: process.env.REACT_APP_API + '/Photos/',
             createOffer: false,
-            filteredItems:[],
-            input:{
+            filteredItems: [],
+            input: {
                 searchKeyword: ""
-            } 
+            }
         }
     }
 
-    addToCart = (ID, BuyerID) =>{
-        if (BuyerID == localStorage.getItem('userID'))
-        {
+    addToCart = (ID, BuyerID) => {
+        if (BuyerID == localStorage.getItem('userID')) {
             console.log('already in cart');
         }
-        else{
+        else {
             const requestOptions = {
-                method: 'POST', 
+                method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 
+                body: JSON.stringify({
                     userID: localStorage.getItem('userID'),
                     foodID: ID
-                    })
-                };
-            fetch(process.env.REACT_APP_API + '/api/cart', requestOptions) 
+                })
+            };
+            fetch(process.env.REACT_APP_API + '/api/cart', requestOptions)
                 .then((response) => response.json())
                 .then(data => {
                     console.log(data);
                 })
 
-                console.log('added');
+            console.log('added');
         }
         window.location.reload(false);
-        this.refreshList();        
+        this.refreshList();
     }
 
 
-    refreshList(){
-        fetch(process.env.REACT_APP_API + '/api/foods/'+ this.state.input.searchKeyword)
-        .then(response => {
-            response.json().then(data => {
-                this.setState(() => {
-                    console.log(data);
-                    return{
-                        filteredItems: data
-                        //foods : data
-                    }
-                })
+    refreshList() {
+        fetch(process.env.REACT_APP_API + '/api/foods/' + this.state.input.searchKeyword)
+            .then(response => {
+                response.json().then(data => {
+                    this.setState(() => {
+                        console.log(data);
+                        return {
+                            filteredItems: data
+                            //foods : data
+                        }
+                    })
+                });
             });
-        });
     }
 
-    onAddFood(){
-        this.setState(() => {return{ createOffer: true}});
+    onAddFood() {
+        this.setState(() => { return { createOffer: true } });
     }
     /*
     handleFoodAdded(){
         this.setState(() => {return{ createOffer: false}});
     }
 */
-/*
-deleteClick(id){
-    if(window.confirm('Are you sure?')){
-    fetch(process.env.REACT_APP_API + '/api/foods/' + id, {
-        method: 'DELETE',
-        headers: {
-            'Accept' : 'application/json',
-            'Content-Type' : 'application/json'
+
+    deleteClick(id) {
+        if (window.confirm('Are you sure?')) {
+            fetch(process.env.REACT_APP_API + '/api/foods/' + id, {
+                method: 'DELETE'
+            }).then((result) => {
+                result.json().then((resp) => {
+                    console.warn(resp)
+                })
+                this.refreshList();
+            })
         }
-    })
-    .then(response => response.json())
-    .then((result) => {
-        alert(result);
-        this.refreshList();
-    }, (error) => {
-        alert ('Failed');
-    })
     }
-}
-*/
 
-inputHandler = (event) => {
-    this.state.input.searchKeyword = event.target.value;
-    this.setState((state) => {return state;});
-    this.refreshList();
-}
-    componentDidMount(){
+
+    inputHandler = (event) => {
+        this.state.input.searchKeyword = event.target.value;
+        this.setState((state) => { return state; });
+        this.refreshList();
+    }
+    componentDidMount() {
         this.refreshList();
     }
 
-    render(){
-        if(localStorage.getItem('userID') == null || localStorage.getItem('userID') == undefined){
+    render() {
+        if (localStorage.getItem('userID') == null || localStorage.getItem('userID') == undefined) {
             return (<div> </div>);
         }
-        return(
+        return (
             <div>
-                
-                {this.state.createOffer && <CreateOffer/>}
-                
-                <button onClick={()=>{this.onAddFood()}}> 
+
+                {this.state.createOffer && <CreateOffer />}
+
+                <button onClick={() => { this.onAddFood() }}>
                     +
                 </button>
                 <form>
-                <input
-                    type="text"
-                    className="search-box"
-                    placeholder="Search for..."
-                    onChange={this.inputHandler}
-                />
-                
-            </form>
+                    <input
+                        type="text"
+                        className="search-box"
+                        placeholder="Search for..."
+                        onChange={this.inputHandler}
+                    />
+
+                </form>
                 <Table className="table">
                     <thead>
                         <tr>
@@ -129,12 +122,12 @@ inputHandler = (event) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.filteredItems.map(food=>
-                            
+                        {this.state.filteredItems.map(food =>
+
                             <tr>
                                 <td>
                                     <img alt="" width="150px" height="150px"
-                                    src={this.state.PhotoPath+food.PhotoFileName} />
+                                        src={this.state.PhotoPath + food.PhotoFileName} />
                                 </td>
                                 <td class="align-middle">{food.Name}</td>
                                 <td class="align-middle">{food.Description}</td>
@@ -144,40 +137,40 @@ inputHandler = (event) => {
                                 <td>
 
                                     {food.OwnerID == localStorage.getItem('userID') ?
-                                    <button type="button" className="btn btn-light mr-1"
-                                        onClick={()=>{this.onUpdateClick()}}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                        </svg>
-                                    </button>
-                                    :null}
+                                        <button type="button" className="btn btn-light mr-1"
+                                            onClick={() => { this.updateClick() }}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                            </svg>
+                                        </button>
+                                        : null}
 
                                     {food.OwnerID == localStorage.getItem('userID') ?
-                                    <button type="button" className="btn btn-light mr-1"
-                                        onClick = {() => {this.deleteClick(food.ID)}}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                                        </svg>
-                                    </button>
-                                    :null}
-                                    
+                                        <button type="button" className="btn btn-light mr-1"
+                                            onClick={() => { this.deleteClick(food.ID) }}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                            </svg>
+                                        </button>
+                                        : null}
+
                                 </td>
 
-                                    <div class="vertical-center">
+                                <div class="vertical-center">
                                     {
                                         food.BuyerID == 0 && food.OwnerID != localStorage.userID &&
-                                        <button onClick={() => this.addToCart(food.ID, food.BuyerID)}> 
+                                        <button onClick={() => this.addToCart(food.ID, food.BuyerID)}>
                                             Add to cart
                                         </button>
                                     }
                                 </div>
-                                
+
                             </tr>)}
                     </tbody>
                 </Table>
             </div>
-           // </div>
+            // </div>
         )
     }
 }
